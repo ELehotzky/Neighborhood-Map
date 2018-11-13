4cf172a028fd7576698cb3030a9891fab13b302d
ELehotzky <Lehotzky.Erica@gmail.com>
2018-11-13 13:52:11 -0600

    added foursquare API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from "./components/Map.js";
+import FourSquareAPI from "./API/";
 
 class App extends Component {
+
+  componentDidMount() {
+    FourSquareAPI.search({
+      near: "Houston, TX",
+      query: "sushi",
+      limit: 10
+    })
+    .then(resp => console.log(resp))
+  }
+
   render() {
     return (
       <div className="App">
