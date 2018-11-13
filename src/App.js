37fdc3c54e9ef76770aaccca065de0371b460bdd
ELehotzky <Lehotzky.Erica@gmail.com>
2018-11-13 14:56:16 -0600

    Revert "added foursquare API"
    This reverts commit 4cf172a028fd7576698cb3030a9891fab13b302d.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from "./components/Map.js";
-import FourSquareAPI from "./API/";
 
 class App extends Component {
-
-  componentDidMount() {
-    FourSquareAPI.search({
-      near: "Houston, TX",
-      query: "sushi",
-      limit: 10
-    })
-    .then(resp => console.log(resp))
-  }
-
   render() {
     return (
       <div className="App">
